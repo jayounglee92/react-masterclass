e5ec52324d7a615c0e8482f259240b8a8b709285
jayounglee92 <lzy6853@gmail.com>
2021-12-27 14:22:48 +0900

    Added Route State

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,8 +1,22 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router";
 import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -12,74 +26,24 @@ const Header = styled.header`
   align-items: center;
 `;
 
-const CoinsList = styled.ul``;
+interface RouteParams {
+  coinId: string;
+}
+interface RouteState {
+  name: string;
+}
 
-const Coin = styled.li`
-  background-color: white;
-  color: ${(props) => props.theme.bgColor};
-  border-radius: 15px;
-  margin-bottom: 10px;
-  a {
-    padding: 20px;
-    transition: color 0.2s ease-in;
-    display: block;
-  }
-  &:hover {
-    a {
-      color: ${(props) => props.theme.accentColor};
-    }
-  }
-`;
-
-const Title = styled.h1`
-  font-size: 48px;
-  color: ${(props) => props.theme.accentColor};
-`;
-
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
-
-function Coins() {
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
   return (
     <Container>
       <Header>
-        <Title>코인</Title>
+        <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? <Loader>Loading...</Loader> : null}
     </Container>
   );
 }
-export default Coins;
+export default Coin;
